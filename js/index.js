268d30739a148c6d9565f26a99455d22eff6520a
Sahaj Bhatt <sahajb97@gmail.com>
2021-08-08 11:42:40 -0400

    Create basic transliteration end point.
    The end point transliterates the input text by using a simple mapping from Gujurati to English.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const Transliterator = require('./transliterate/transliterator');
+const transliterator = new Transliterator();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/transliterate', (req, res) => {
-  console.log('something else');
+  const result =
+    transliterator.transliterate('તમે કેમ છો', 'gujurati', 'english');
+  if (result.error) {
+    console.log(result.error);
+    res.send(result);
+  } else {
+    console.log(result.output);
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
