f32efd400bf2ceab13118a2a59dc4f2cc286faa0
Sahaj Bhatt <sahajb97@gmail.com>
2021-10-25 22:29:03 -0400

    Read text, source, and destination from query.
    Also added new helpful modules.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 const port = process.env.port || 3000;
 const Transliterator = require('./transliterate/transliterator');
 const transliterator = new Transliterator();
+
+// Adding Helmet to enhance your API's security
+app.use(helmet());
+
+// Enabling CORS for all requests
+app.use(cors());
+
+// Adding morgan to log HTTP requests
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/transliterate', (req, res) => {
+  const text = req.query.text;
+  const src = req.query.src;
+  const dest = req.query.dest;
   const result =
-    transliterator.transliterate('સત્સંગ', 'gujurati', 'english');
+    transliterator.transliterate(text, src, dest);
   if (result.error) {
     console.log(result.error);
-    res.send(result);
   } else {
     console.log(result.output);
-    res.send(result);
   }
+  res.send(result);
 });
 
 app.listen(port, () => {
